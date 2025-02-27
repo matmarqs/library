693e14aa195f93eb33ec51362f145262ed9ce548
Mateus Marques <mateusmarques2001@usp.br>
2025-02-27 14:15:16 -0300

    Simple skeleton. Have to implement a form, which idk yet

---
+++ main.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book() {
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+function displayBooks() {
+  myLibrary.forEach(book => {
+    //display them in some sort of table, or cards
+  });
+}
